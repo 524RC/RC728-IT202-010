c6926aec3af721d5f2d6f13afee0ad44e0526fba
Ryan Chung <Rchung524@gmail.com>
2026-05-03 17:50:44 -0400

    commenting out js for edit_anime to see php validation

--- public_html/project/admin/edit_anime.php
+++ public_html/project/admin/edit_anime.php
@@ -114,7 +114,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     </p>
 </div>
 <script>
-
+/*
 function validate(form) {
     let title = form.title.value.trim();
     let pictureUrl = form.picture_url.value.trim();
@@ -142,6 +142,7 @@ function validate(form) {
 
     return true;
 }
+*/
 </script>
 
 <?php require(__DIR__ . "/../../../partials/flash.php"); ?>
